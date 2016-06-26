9070e2fa83488c542ca8c6e5a3cb2954201da115
Felipe <Felipe@10.5.88.147>
2016-06-25 22:36:04 -0300

    Modificações controles

---
+++ src/java/br/edu/ifsul/controle/ControleRelatorios.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.controle;
+
+
+
+import br.edu.ifsul.dao.PessoaDAO;
+import br.edu.ifsul.dao.PostagemDAO;
+import br.edu.ifsul.modelo.Pessoa;
+import br.edu.ifsul.modelo.Postagem;
+import br.edu.ifsul.util.UtilRelatorios;
+import java.io.Serializable;
+import java.util.HashMap;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Felipe
+ */
+@ManagedBean(name = "controleRelatorios")
+@SessionScoped
+public class ControleRelatorios implements Serializable {
+    @EJB
+    private PessoaDAO<Pessoa> daoPessoa;
+    @EJB
+    private PostagemDAO<Postagem> daoPostagem;
+    
+    public ControleRelatorios() {
+        
+    }
+    public void imprimeRelatorioPessoas() {
+        HashMap parametros = new HashMap();
+        UtilRelatorios.imprimeRelatorio("relatorioPessoas", parametros, daoPessoa.getListaTodos());
+    }
+    public void imprimeRelatorioPostagens() {
+        HashMap parametros = new HashMap();
+        UtilRelatorios.imprimeRelatorio("relatorioPostagens", parametros, daoPostagem.getListaTodos());
+    }
+
+}
