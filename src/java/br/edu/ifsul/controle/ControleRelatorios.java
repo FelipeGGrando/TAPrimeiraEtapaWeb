538e5f27a7606bab4457c0a089f9e7ba71fc6b83
Felipe <Felipe@10.5.88.147>
2016-06-26 12:43:03 -0300

    Modificações controles

--- src/java/br/edu/ifsul/controle/ControleRelatorios.java
+++ src/java/br/edu/ifsul/controle/ControleRelatorios.java
@@ -5,12 +5,12 @@
  */
 package br.edu.ifsul.controle;
 
-
-
 import br.edu.ifsul.dao.PessoaDAO;
 import br.edu.ifsul.dao.PostagemDAO;
+import br.edu.ifsul.dao.VideoDAO;
 import br.edu.ifsul.modelo.Pessoa;
 import br.edu.ifsul.modelo.Postagem;
+import br.edu.ifsul.modelo.Video;
 import br.edu.ifsul.util.UtilRelatorios;
 import java.io.Serializable;
 import java.util.HashMap;
@@ -25,21 +25,31 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean(name = "controleRelatorios")
 @SessionScoped
 public class ControleRelatorios implements Serializable {
+
     @EJB
     private PessoaDAO<Pessoa> daoPessoa;
     @EJB
+    private VideoDAO<Video> daoVideo;
+    @EJB
     private PostagemDAO<Postagem> daoPostagem;
-    
+
     public ControleRelatorios() {
-        
+
     }
+
     public void imprimeRelatorioPessoas() {
         HashMap parametros = new HashMap();
         UtilRelatorios.imprimeRelatorio("relatorioPessoas", parametros, daoPessoa.getListaTodos());
     }
+
     public void imprimeRelatorioPostagens() {
         HashMap parametros = new HashMap();
         UtilRelatorios.imprimeRelatorio("relatorioPostagens", parametros, daoPostagem.getListaTodos());
+    }
+
+    public void imprimeRelatorioVideos() {
+        HashMap parametros = new HashMap();
+        UtilRelatorios.imprimeRelatorio("relatorioVideos", parametros, daoVideo.getListaTodos());
     }
 
     public PessoaDAO<Pessoa> getDaoPessoa() {
@@ -56,6 +66,14 @@ public class ControleRelatorios implements Serializable {
 
     public void setDaoPostagem(PostagemDAO<Postagem> daoPostagem) {
         this.daoPostagem = daoPostagem;
+    }
+
+    public VideoDAO<Video> getDaoVideo() {
+        return daoVideo;
+    }
+
+    public void setDaoVideo(VideoDAO<Video> daoVideo) {
+        this.daoVideo = daoVideo;
     }
 
 }
